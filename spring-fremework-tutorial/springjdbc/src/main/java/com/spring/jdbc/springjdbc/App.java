c80ca179ea59d25ed45274747b7608b66a269d50
Sumanta <sumanta.ghosh.mit@gmail.com>
2023-11-25 15:35:04 +0530

    Config using  XML and insert data in DB

--- spring-fremework-tutorial/springjdbc/src/main/java/com/spring/jdbc/springjdbc/App.java
+++ spring-fremework-tutorial/springjdbc/src/main/java/com/spring/jdbc/springjdbc/App.java
@@ -1,13 +1,27 @@
 package com.spring.jdbc.springjdbc;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/spring/jdbc/springjdbc/app-config.xml")) {
+			JdbcTemplate db = ctx.getBean("jdbctemplate", JdbcTemplate.class);
+
+			// insert query
+			String query = "INSERT INTO student(id,name,city) values(?,?,?)";
+			int rs = db.update(query, 2, "Saheb", "Kotulpur");
+			System.out.println("Number of reord inserted ... " + rs);
+
+		} catch (BeansException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		;
+	}
 }
