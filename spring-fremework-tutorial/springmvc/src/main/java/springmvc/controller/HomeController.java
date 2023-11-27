75e2216349bc5623e8c989408792b0492354bc64
Sumanta <sumanta.ghosh.mit@gmail.com>
2023-11-27 17:12:25 +0530

    Create controller

---
+++ spring-fremework-tutorial/springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("This is home url...");
+		return "index";
+	}
+
+}
