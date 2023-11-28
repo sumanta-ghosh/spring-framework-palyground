f29928c28060f7576041d1c7a1731c0ff0334fa9
Sumanta <sumanta.ghosh.mit@gmail.com>
2023-11-28 18:22:02 +0530

    Modify RequestMapping

--- spring-fremework-tutorial/springmvc/src/main/java/springmvc/controller/HomeController.java
+++ spring-fremework-tutorial/springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -6,12 +6,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 		System.out.println("This is home url...");
 		model.addAttribute("pageName", "Home..");
@@ -23,7 +24,7 @@ public class HomeController {
 		return "index";
 	}
 
-	@RequestMapping("/about")
+	@RequestMapping(path = "/about", method = RequestMethod.GET)
 	public ModelAndView about() {
 		System.out.println("This is about url...");
 		ModelAndView mv = new ModelAndView();
