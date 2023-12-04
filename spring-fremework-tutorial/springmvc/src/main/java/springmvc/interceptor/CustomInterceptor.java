4598e49deb9387bc48ec7f32fb48a667c8347659
Sumanta <sumanta.ghosh.mit@gmail.com>
2023-12-05 00:17:37 +0530

    Implement preHandle interceptor

--- spring-fremework-tutorial/springmvc/src/main/java/springmvc/interceptor/CustomInterceptor.java
+++ spring-fremework-tutorial/springmvc/src/main/java/springmvc/interceptor/CustomInterceptor.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class CustomInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
+		System.out.println("This is preHandle");
 		return true;
 	}
 }
